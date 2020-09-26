81bbc2d5eb4c7f929d508d3835126f78a972aae7
Anastasiya Zavrazhneva <zavrazhneva.a@gmail.com>
2020-09-26 21:59:32 +0300

    feat: add hanoi-tower function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,8 +1,7 @@
-const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
     if (!Array.isArray(arr)) {
-        throw new CustomError();
+        throw new Error();
     }
     let newArr = [];
     for (let i = 0; i < arr.length; ++i) {
