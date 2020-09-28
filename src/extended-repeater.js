ade79fe03fc76a1be73b70b3766040bb000e52ac
Anastasiya Zavrazhneva <zavrazhneva.a@gmail.com>
2020-09-28 23:12:42 +0300

    add recurcive-depth function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports =  function repeater( str, options ) {
+
+  str = String(str);
+
+  function converter(arr, separator ) {
+    if(separator === undefined) {
+      return arr.join('+')
+    }
+    return arr.join(separator)
+  }
+
+  function creatureArr (str = '' , current = 1, separator ) {
+
+    str = String(str);
+    let creatureArrNew = [];
+    for(let i = 0; i < current; i++) {
+      creatureArrNew.push(str)
+    }
+    return converter(creatureArrNew, separator)
+  }
+
+  let newString = str + creatureArr(options.addition, options.additionRepeatTimes, options.additionSeparator);
+  let resultString = creatureArr(newString, options.repeatTimes, options.separator )
+
+  return resultString;
+
 };
-  
